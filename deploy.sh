a16cefb5ac87fc1b1deb5d94fa5b7df6b7543d70
Krzysztof Łos <krzysztof.los@vp.pl>
2017-04-04 11:42:26 +0200

    fix test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 cd src
 MY_RAND=`shuf -i100000000-999999999 -n1`
-cat schools.html | sed  s/__SCHOOLS_ID__/$MY_RAND/ > schools2.html
+cat schools.html | sed  s/5c418/$MY_RAND/ > schools2.html
 mv schools2.html schools.html
 zip ../src.zip ./ -r
 cd ..
